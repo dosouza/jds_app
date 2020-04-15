a58c777618cd3be7c569bf04ee4f96de882f72be
Demetrius O Souza <dosouzasp@gmail.com>
2020-04-15 11:21:34 -0300

    Treinamento de Listas

--- lib/listas_page.dart
+++ lib/listas_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ListasPage extends StatefulWidget {
-
-  List<String> item = <String> [
-    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','Z'
-  ];
-
   @override
   _ListasPageState createState() =>  _ListasPageState();
 }
 
 class _ListasPageState extends State<ListasPage> {
+
+  List<String> items = <String> [
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','Z'
+  ];
+
   @override
   Widget build(BuildContext context) {
+
+    Iterable<Widget> listTiles = items.map((String item) => buildListTile(context, item));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Listas Page'),
@@ -25,8 +28,24 @@ class _ListasPageState extends State<ListasPage> {
         ],
       ),
       body: Scrollbar(
-        child: ListView(),
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          children: listTiles.toList(),
+        ),
       ),
     );
   }
+}
+
+Widget buildListTile(BuildContext context, String item) {
+  Widget secondary = const Text(
+      'Texto Secundario'
+  );
+
+  return MergeSemantics(
+    child: ListTile(
+      title: Text('Este item representa a letra $item.'),
+      subtitle: secondary,
+    ),
+  );
 }
